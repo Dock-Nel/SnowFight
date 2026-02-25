64a90d3df35fd644b3a6cef52e39d0a59c3bcdf4
Mael <mael.lemmy2007@gmail.com>
2026-02-25 14:43:49 +0100

    Replaced Raycast by Linecast
    Fixes some issues with the AI not detecting the player properly

--- Assets/Scripts/AIBehaviour.cs
+++ Assets/Scripts/AIBehaviour.cs
@@ -16,12 +16,17 @@ public class AIBehaviour : MonoBehaviour
 
     //Variables
     [SerializeField] bool Alive = true;
-    [SerializeField] bool PlayerDetection = false;
-    [SerializeField] bool Agressivity = false;
+    [SerializeField] int Snowball = 3;
 
+    [SerializeField] bool PlayerDetection = false;
+    [SerializeField] bool Aggressivity = false;
+    [SerializeField] float PlayerDistance;
+
+    //Constants 
     [SerializeField] int Speed;
     [SerializeField] int Gravity;
-    [SerializeField] float PlayerDistance;
+    [SerializeField] int Range = 20;
+    
 
     void Awake()
     {
@@ -31,24 +36,38 @@ public class AIBehaviour : MonoBehaviour
     
     void Update()
     {
+        //Player detection system
         PlayerDistance = Vector3.Distance(transform.position, Player.transform.position);
-        if (PlayerDistance < 10)
+        if (PlayerDistance < Range)
         {
-            PlayerDetected();
-        }
-    }
-
-    void PlayerDetected()
-    {
-        ray = new Ray(transform.position, Player.transform.position);
-        Debug.DrawLine(transform.position, Player.transform.position, Color.red);
-        if (!Physics.Raycast(ray, out hit))
-        {
-            Debug.Log("Player Detected");
+            Raycast();
+            PlayerDetection = true;
         }
         else
         {
+            PlayerDetection = false;
+        }
+    }
+
+    //Raycast function
+    void Raycast()
+    {
+        //Raycast Calculation
+        ray = new Ray(transform.position, Player.transform.position);
+        Debug.DrawLine(transform.position, Player.transform.position, Color.red); //Makes the Ray visible, debug feature only
+
+        if (!Physics.Linecast(transform.position, Player.transform.position)) //If Ray reaches Player
+        {
+            Aggressivity = true;
+            agent.destination = transform.position;
+
+            Debug.Log("test");
+        }
+        else
+        {
+            agent.destination = Player.transform.position;
+
             Debug.Log("Player in range, not detected");
-        }  
+        }
     }
 }
