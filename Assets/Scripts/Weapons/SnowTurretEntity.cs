4f6a21423d04e720715f6fcd62639c19479c2901
nelson.bidault-ferre2.auditeur <nelson.bidault-ferre2.auditeur@lecnam.net>
2026-05-26 15:33:54 +0200

    Fix : Turrets not working

--- Assets/Scripts/Weapons/SnowTurretEntity.cs
+++ Assets/Scripts/Weapons/SnowTurretEntity.cs
@@ -26,6 +26,8 @@ public class SnowTurretEntity : MonoBehaviour
     {
         Rigidbody sb = Instantiate(snowballPrefab, shootPoint.position, shootPoint.rotation);
         sb.gameObject.SetActive(true);
+        Snowballs snowballScript = sb.GetComponent<Snowballs>();
+        snowballScript.Source = "Player";
         Vector3 force = (transform.forward * shootForce) + (Vector3.up * upwardForce);
         sb.AddForce(force, ForceMode.Impulse);
     }
