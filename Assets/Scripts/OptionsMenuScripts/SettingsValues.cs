edc1482985c049edfd803053517c552a4ce9f7ca
Selto86 <mael.lemmy2007@gmail.com>
2026-06-13 21:11:22 +0200

    Update Settings

--- Assets/Scripts/OptionsMenuScripts/SettingsValues.cs
+++ Assets/Scripts/OptionsMenuScripts/SettingsValues.cs
@@ -1,18 +1,29 @@
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
-using static UnityEditor.PlayerSettings;
 using static UnityEngine.Rendering.DebugUI;
 
 public class SettingsValues : MonoBehaviour
 {
+    public static SettingsValues instance;
+
     [Header("Graphics")]
+    [Header("MotionBlur")]
+    [SerializeField] Toggle MotionBlurToggle;
+    [SerializeField] VolumeProfile volume;
+    MotionBlur Blur;
+    public bool MotionBlurBool;
     [Header("VSync")]
     [SerializeField] Toggle VSyncToggle;
+    public bool VSyncBool;
     [Header("FPSCap")]
     [SerializeField] Toggle FPSCapToggle;
     [SerializeField] Slider FPSCapSlider;
+    public bool FPSCapBool;
+    public float FPSCapValue;
 
     [Header("Audio")]
     [SerializeField] AudioMixer Mixer;
@@ -20,6 +31,7 @@ public class SettingsValues : MonoBehaviour
     [SerializeField] Slider MasterVolumeSlider;
     [Header("Music")]
     [SerializeField] Toggle MusicToggle;
+    [SerializeField] AudioSource MusicSource;
 
     [Header("Accesibility")]
     [Header("Sensitivity")]
@@ -30,20 +42,90 @@ public class SettingsValues : MonoBehaviour
     void Start()
     {
         DontDestroyOnLoad(gameObject);
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            //This part makes it so nothing is reset once you go back to the main menu
+
+            
+            if (instance.MotionBlurBool)
+            {
+                MotionBlurToggle.isOn = true;
+            }
+            else
+            {
+                MotionBlurToggle.isOn = false;
+            }
+            instance.MotionBlurToggle = MotionBlurToggle;
+            instance.volume = volume;
+            instance.Blur = Blur;
+
+            if (instance.VSyncBool)
+            {
+                VSyncToggle.isOn = true;
+            }
+            else
+            {
+                VSyncToggle.isOn = false;
+            }
+            instance.VSyncToggle = VSyncToggle;
+
+            FPSCapSlider.value = FPSCapValue;
+            instance.FPSCapToggle = FPSCapToggle;
+            instance.FPSCapSlider = FPSCapSlider;
+            if (instance.FPSCapBool)
+            {
+                instance.FPSCapToggle.isOn = true;
+            }
+            else
+            {
+                instance.FPSCapToggle.isOn = false;
+            }
+            instance.FPSCapSlider.value = instance.FPSCapValue;
+
+
+            instance.Mixer = Mixer;
+            instance.MasterVolumeSlider = MasterVolumeSlider;
+            instance.MusicToggle = MusicToggle;
+            instance.MusicSource = MusicSource;
+            instance.SensitivitySlider = SensitivitySlider;
+            instance.SnowEffectToggle = SnowEffectToggle;
+            Destroy(gameObject);
+            return;
+        }
     }
 
     //Graphics Settings
+    public void MotionBlur()
+    {
+        volume.TryGet<MotionBlur>(out Blur);
+        if (MotionBlurToggle.isOn)
+        {
+            Blur.intensity.value = 1f;
+            MotionBlurBool = true;
+        }
+        else
+        {
+            Blur.intensity.value = 0f;
+            MotionBlurBool = false;
+        }
+    }
+
     public void VSync()
     {
         if (VSyncToggle.isOn)
         {
-            Debug.Log("VSyncOn");
             QualitySettings.vSyncCount = 1;
+            VSyncBool = true;
         }
         else
         {
-            Debug.Log("VSyncOff");
             QualitySettings.vSyncCount = 0;
+            VSyncBool = false;
         }
     }
 
@@ -51,13 +133,14 @@ public class SettingsValues : MonoBehaviour
     {
         if (FPSCapToggle.isOn)
         {
-            Debug.Log("FPSCapOn : " + FPSCapSlider.value);
-            Application.targetFrameRate = (int)FPSCapSlider.value;
+            FPSCapValue = FPSCapSlider.value;
+            Application.targetFrameRate = (int)FPSCapValue;
+            FPSCapBool = true;
         }
         else
         {
-            Debug.Log("FPSCapOff");
             Application.targetFrameRate = 0;
+            FPSCapBool = false;
         }
     }
 
@@ -66,6 +149,18 @@ public class SettingsValues : MonoBehaviour
     {
         int VolumeValue = (((int)MasterVolumeSlider.value * 80) / 100) - 80; //Converts the percentage to a range between -80 to 0 (to be put as a dB value)
         Debug.Log(VolumeValue);
-        Mixer.SetFloat("", VolumeValue);
+        Mixer.SetFloat("MasterVolume", VolumeValue);
+    }
+
+    public void Music()
+    {
+        if (MusicToggle.isOn)
+        {
+            MusicSource.Play();
+        }
+        else
+        {
+            MusicSource.Stop();
+        }
     }
 }
